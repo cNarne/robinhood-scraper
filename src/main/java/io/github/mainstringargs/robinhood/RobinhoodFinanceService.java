cb31fd78ba5d75e80ebdc4b3d2f42a5dd1128426
mainstringargs <mark.l.cloutier@gmail.com>
2018-11-28 15:49:29 -0600

    Updating for compliance with stock-data-spi 1.1

--- src/main/java/io/github/mainstringargs/robinhood/RobinhoodFinanceService.java
+++ src/main/java/io/github/mainstringargs/robinhood/RobinhoodFinanceService.java
@@ -22,6 +22,7 @@ public class RobinhoodFinanceService
 
   /** The api. */
   RobinhoodApi api = null;
+  private boolean isInitialized = false;
 
   /*
    * (non-Javadoc)
@@ -30,7 +31,11 @@ public class RobinhoodFinanceService
    */
   public void init() {
 
-    api = new RobinhoodApi();
+    if (!isInitialized) {
+      api = new RobinhoodApi();
+      isInitialized = true;
+    }
+
 
   }
 
@@ -46,72 +51,35 @@ public class RobinhoodFinanceService
   /*
    * (non-Javadoc)
    * 
+   * @see io.github.mainstringargs.stockData.spi.StockDataService#getShortServiceName()
+   */
+  @Override
+  public String getShortServiceName() {
+    return "RH";
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see io.github.mainstringargs.stockData.spi.StockDataService#getStockData(java.lang.String)
+   */
+  @Override
+  public Map<String, Object> getStockData(String ticker) {
+    return getRobinhoodStockData(ticker);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see io.github.mainstringargs.stockData.spi.StockDataService#getStockData(java.lang.String[])
    */
   public Map<String, Map<String, Object>> getStockData(String... tickers) {
 
     Map<String, Map<String, Object>> allStockData = new HashMap<String, Map<String, Object>>();
 
-
-
     for (String symbol : tickers) {
 
-
-      Map<String, Object> stockData = new LinkedHashMap<String, Object>();
-
-      TickerQuote quote = null;
-      try {
-        quote = api.getQuoteByTicker(symbol);
-      } catch (TickerNotFoundException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-      }
-
-      RatingList ratingList = null;
-      try {
-        ratingList = api.getRatingsByTickers(symbol);
-      } catch (RequestTooLargeException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-
-      if (quote != null)
-        stockData.put("Last Trade Price", quote.getLastTradePrice());
-
-      if (ratingList != null) {
-        List<Rating> resultList = ratingList.getResults();
-
-        if (resultList != null) {
-          Rating singleRating = resultList.get(0);
-
-          if (singleRating != null && singleRating.getSummary() != null) {
-
-
-            double total = singleRating.getSummary().getNumBuyRatings()
-                + singleRating.getSummary().getNumHoldRatings()
-                + singleRating.getSummary().getNumSellRatings();
-
-            stockData.put("Num Buy Ratings", singleRating.getSummary().getNumBuyRatings());
-
-            stockData.put("Buy Rating Percentage",
-                singleRating.getSummary().getNumBuyRatings() / total);
-
-
-            stockData.put("Num Hold Ratings", singleRating.getSummary().getNumHoldRatings());
-
-            stockData.put("Hold Rating Percentage",
-                singleRating.getSummary().getNumHoldRatings() / total);
-
-            stockData.put("Num Sell Ratings", singleRating.getSummary().getNumSellRatings());
-
-            stockData.put("Sell Rating Percentage",
-                singleRating.getSummary().getNumSellRatings() / total);
-
-
-
-          }
-        }
-      }
+      Map<String, Object> stockData = getRobinhoodStockData(symbol);
 
       allStockData.put(symbol.toUpperCase(), stockData);
 
@@ -120,6 +88,77 @@ public class RobinhoodFinanceService
     return allStockData;
 
   }
+
+  /**
+   * Gets the robinhood stock data.
+   *
+   * @param symbol the symbol
+   * @return the robinhood stock data
+   */
+  private Map<String, Object> getRobinhoodStockData(String symbol) {
+
+    if (!isInitialized) {
+      init();
+    }
+
+    Map<String, Object> stockData = new LinkedHashMap<String, Object>();
+
+    TickerQuote quote = null;
+    try {
+      quote = api.getQuoteByTicker(symbol);
+    } catch (TickerNotFoundException e1) {
+      e1.printStackTrace();
+    }
+
+    RatingList ratingList = null;
+    try {
+      ratingList = api.getRatingsByTickers(symbol);
+    } catch (RequestTooLargeException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    if (quote != null)
+      stockData.put("Last Trade Price", quote.getLastTradePrice());
+
+    if (ratingList != null) {
+      List<Rating> resultList = ratingList.getResults();
+
+      if (resultList != null) {
+        Rating singleRating = resultList.get(0);
+
+        if (singleRating != null && singleRating.getSummary() != null) {
+
+
+          double total = singleRating.getSummary().getNumBuyRatings()
+              + singleRating.getSummary().getNumHoldRatings()
+              + singleRating.getSummary().getNumSellRatings();
+
+          stockData.put("Num Buy Ratings", singleRating.getSummary().getNumBuyRatings());
+
+          stockData.put("Buy Rating Percentage",
+              singleRating.getSummary().getNumBuyRatings() / total);
+
+
+          stockData.put("Num Hold Ratings", singleRating.getSummary().getNumHoldRatings());
+
+          stockData.put("Hold Rating Percentage",
+              singleRating.getSummary().getNumHoldRatings() / total);
+
+          stockData.put("Num Sell Ratings", singleRating.getSummary().getNumSellRatings());
+
+          stockData.put("Sell Rating Percentage",
+              singleRating.getSummary().getNumSellRatings() / total);
+
+
+
+        }
+      }
+    }
+    return stockData;
+  }
+
+
 
   // public Map<String, Map<String, Object>> getStockData() {
   // return null;
